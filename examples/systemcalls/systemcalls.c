515ac1dfbcec50943766c5e2b881602eac92482b
maditakruithof <madita.kruithof@sioux.eu>
2024-08-19 14:38:38 +0200

    assignment 3 part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,4 +1,13 @@
 #include "systemcalls.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
 
 /**
  * @param cmd the command to execute with system()
@@ -9,15 +18,7 @@
 */
 bool do_system(const char *cmd)
 {
-
-/*
- * TODO  add your code here
- *  Call the system() function with the command set in the cmd
- *   and return a boolean true if the system() call completed with success
- *   or false() if it returned a failure
-*/
-
-    return true;
+    return system(cmd) == 0;
 }
 
 /**
@@ -45,9 +46,36 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
+
+    pid_t pid = fork();
+    if (pid == -1) {
+        perror("fork failed");
+        va_end(args);
+        return false;
+    }
+
+    if (pid == 0) {
+        // Child process: Execute the command
+        execv(command[0], command);
+
+        // If execv returns, an error occurred
+        perror("execv failed");
+        return EXIT_FAILURE;
+    } 
+    else {
+        // Parent process: Wait for the child to complete
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid failed");
+            va_end(args);
+            return false;
+        }
+
+        // Check if the child process terminated normally
+        va_end(args);
+        return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+    }
+
 
 /*
  * TODO:
@@ -80,20 +108,45 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
+    pid_t pid = fork();
+    if (pid == -1) 
+    {
+        perror("fork failed");
+        va_end(args);
+        return false;
+    }
 
-/*
- * TODO
- *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
- *   redirect standard out to a file specified by outputfile.
- *   The rest of the behaviour is same as do_exec()
- *
-*/
+    if (pid == 0)
+    {
+        int fd = open(outputfile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (fd == -1) {
+            perror("open failed");
+            return EXIT_FAILURE;
+        }
 
-    va_end(args);
+        if (dup2(fd, STDOUT_FILENO) == -1) {
+            perror("dup2 failed");
+            close(fd);
+            return EXIT_FAILURE;
+        }
 
-    return true;
+        close(fd);
+
+        execv(command[0], command);
+
+        perror("execv failed");
+        return EXIT_FAILURE;
+    } 
+    else {
+        int status;
+        if (waitpid(pid, &status, 0) == -1) {
+            perror("waitpid failed");
+            va_end(args);
+            return false;
+        }
+
+        va_end(args);
+        return WIFEXITED(status) && WEXITSTATUS(status) == 0;
+    }
 }
--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -48,9 +48,6 @@ then
 		exit 1
 	fi
 fi
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
 
 for i in $( seq 1 $NUMFILES)
 do
