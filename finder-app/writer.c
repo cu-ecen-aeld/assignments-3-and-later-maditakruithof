109ce20071729d6fc5a4f8d8cd48e5767465a6d4
maditakruithof <madita.kruithof@sioux.eu>
2024-08-19 09:17:00 +0200

    Creation of writer application

---
+++ finder-app/writer.c
@@ -0,0 +1,20 @@
+#include <syslog.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    if(argc < 2)
+    {
+        syslog(LOG_ERR , "Not enough arguments");
+        return 1;
+    }
+    else
+    {
+        syslog(LOG_DEBUG, "Writing %s to %s", argv[2], argv[1]);
+        FILE *fptr;
+        fptr = fopen(argv[1], "w");
+        fprintf(fptr, "%s", argv[2]);
+        fclose(fptr);
+    }
+    return 0;
+}
